eea586468b45bfe4808c307172044820f06ff072
Thijmen <Thijmenh01@gmail.com>
2020-03-10 09:16:00 +0100

    Recoild added & shooting fixed

--- Project3_Periode7_Unity/Assets/Assets/Programming/Scripts/Rik/PlayerGun.cs
+++ Project3_Periode7_Unity/Assets/Assets/Programming/Scripts/Rik/PlayerGun.cs
@@ -2,56 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerGun : MonoBehaviour
-{
-    private Player player;
+public class PlayerGun : MonoBehaviour {
+    [SerializeField] private float fireRate = 7.5f;
+    [SerializeField] private PlayerScript player;
     [SerializeField] private GameObject bullet;
     private float shootTimer;
+    private float nextTimeToFire = 0f;
 
-    private void Update()
-    {
+    private void Start() {
+        player = transform.parent.GetComponentInParent<PlayerScript>();
+    }
+
+    private void Update() {
         MoveGun();
 
-        if (Input.GetMouseButton(0))
-        {
-            if (PlayerScript.instance.playerDir != PlayerScript.PlayerDir.Neutral)
-            {
+        if(PlayerScript.instance.playerDir != PlayerScript.PlayerDir.Neutral) {
+            if(Input.GetMouseButton( 0 ) && Time.time >= nextTimeToFire) {
                 Shoot();
+                Recoil();
             }
         }
     }
 
-    private void MoveGun()
-    {
-        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+    private void MoveGun() {
+        Ray cameraRay = Camera.main.ScreenPointToRay( Input.mousePosition );
         float rayLength;
-        Plane groundPlane = new Plane(Vector3.forward, Vector3.zero);
+        Plane groundPlane = new Plane( Vector3.forward , Vector3.zero );
 
-        if (groundPlane.Raycast(cameraRay, out rayLength))
-        {
-            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+        if(groundPlane.Raycast( cameraRay , out rayLength )) {
+            Vector3 pointToLook = cameraRay.GetPoint( rayLength );
 
-            if (PlayerScript.instance.playerDir != PlayerScript.PlayerDir.Neutral)
-            {
+            if(PlayerScript.instance.playerDir != PlayerScript.PlayerDir.Neutral) {
                 //if (transform.eulerAngles.x < 50f && transform.eulerAngles.x > -50)
                 //{
-                transform.LookAt(new Vector3(pointToLook.x, pointToLook.y, transform.position.z));
+                transform.LookAt( new Vector3( pointToLook.x , pointToLook.y , transform.position.z ) );
                 //}
             }
         }
     }
 
-    private void Shoot()
-    {
-        if (shootTimer <= 0f)
-        {
-            GameObject Playerbullet = Instantiate(bullet, transform.GetChild(0).transform.position, Quaternion.identity);
-            Playerbullet.tag = "PlayerBullet";
-            shootTimer = 0.3f;
+    private void Shoot() {
+        GameObject Playerbullet = Instantiate( bullet , transform.GetChild( 0 ).transform.position , Quaternion.identity );
+        Playerbullet.tag = "PlayerBullet";  
+        nextTimeToFire = Time.time + 1f / fireRate;
+    }
+
+    private void Recoil() {
+        Vector2 pos = player.transform.position;
+        if(PlayerScript.instance.playerDir == PlayerScript.PlayerDir.Left) {
+            pos.x = Mathf.Lerp( player.transform.position.x , player.transform.position.x + 5 , 1f * Time.deltaTime );
         }
-        else
-        {
-            shootTimer -= Time.deltaTime;
+
+        if(PlayerScript.instance.playerDir == PlayerScript.PlayerDir.Right) {
+            pos.x = Mathf.Lerp( player.transform.position.x , player.transform.position.x - 5 , 1f * Time.deltaTime );
         }
+        player.transform.position = pos;
     }
 }
