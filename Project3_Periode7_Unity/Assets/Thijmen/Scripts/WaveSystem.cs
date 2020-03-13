7fb3af246a53a454f29667f767437aabef342c1a
Thijmen <Thijmenh01@gmail.com>
2020-03-13 13:25:01 +0100

    wave system

--- Project3_Periode7_Unity/Assets/Thijmen/Scripts/WaveSystem.cs
+++ Project3_Periode7_Unity/Assets/Thijmen/Scripts/WaveSystem.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveSystem : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class WaveSystem : MonoBehaviour {
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private Transform spawnpoint;
+    [SerializeField] private List<GameObject> enemiesAlive;
+
+    private float spawnInterval = 0.25f;
+    private int amountOfEnemies;
+    private int currentWave;
+
+    private void Update() {
+        if(Input.GetKeyDown( KeyCode.F )) {
+            amountOfEnemies = 10;
+            SpawnWave();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void SpawnWave() {
+        StartCoroutine( SpawnSingleEnemy() );
+        currentWave++;
+    }
+
+    private IEnumerator SpawnSingleEnemy() {
+        for(int i = 0; i < amountOfEnemies; i++) {
+            GameObject enemyAddToList = Instantiate( enemy , spawnpoint.position , Quaternion.identity );
+            enemiesAlive.Add( enemyAddToList );
+            yield return new WaitForSeconds( spawnInterval );
+        }
     }
 }
